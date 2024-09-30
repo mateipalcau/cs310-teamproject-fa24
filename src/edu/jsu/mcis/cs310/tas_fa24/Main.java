91ae18e87e528004cced0337ba309cdfd0ecffbd
Matthew <matthewbright3000@gmail.com>
2024-09-30 12:05:28 -0500

    test message on line 10

--- src/edu/jsu/mcis/cs310/tas_fa24/Main.java
+++ src/edu/jsu/mcis/cs310/tas_fa24/Main.java
@@ -7,7 +7,7 @@ import java.time.temporal.ChronoUnit;
 import java.time.format.DateTimeFormatter;
 //jack test
 public class Main {
-
+//matthew
     public static void main(String[] args) {
         
         // test database connectivity; get DAOs
