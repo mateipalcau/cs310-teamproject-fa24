a2611ea9c6bba96e8c229fb5d3298aea55ca91f6
Garret <ghowell2@stu.jsu.edu>
2024-10-07 12:06:56 -0500

    Added Punch constructors

--- src/edu/jsu/mcis/cs310/tas_fa24/dao/PunchDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa24/dao/PunchDAO.java
@@ -1,6 +1,15 @@
 
 package edu.jsu.mcis.cs310.tas_fa24.dao;
 
+import edu.jsu.mcis.cs310.tas_fa24.EventType;
+import java.time.LocalDateTime;
+import edu.jsu.mcis.cs310.tas_fa24.Badge;
 public class PunchDAO {
-    
+
+    public void Punch(int terminalid, Badge badge, EventType punchType) {
+        
+    }
+    public void Punch(int id, int terminalid, Badge badge, LocalDateTime originaltimestamp, EventType punchType) {
+        
+    }
 }
