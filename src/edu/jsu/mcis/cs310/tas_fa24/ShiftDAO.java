a23c0bad045c74522dfc8f9c42ad96602261a7dd
Matthew <matthewbright3000@gmail.com>
2024-10-07 11:45:09 -0500

    Changed name of Shift.java to ShiftDAO.java ShiftDAO has data fields, and constructor has been started

---
+++ src/edu/jsu/mcis/cs310/tas_fa24/ShiftDAO.java
@@ -0,0 +1,12 @@
+package edu.jsu.mcis.cs310.tas_fa24;
+import java.time.*;
+
+public class ShiftDAO {
+    public LocalTime startShift, endShift;
+    public int lunchDuration, shiftDuration;
+    
+    public ShiftDAO()
+    {
+        
+    }
+}
