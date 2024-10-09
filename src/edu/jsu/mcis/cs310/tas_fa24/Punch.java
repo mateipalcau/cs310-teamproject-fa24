809e823e2d88caaf2691c1485d377e59596cc499
Matei Palcau <mpalcau@stu.jsu.edu>
2024-10-09 11:15:48 -0500

    added things to Punch.java and PunchDAO.java

--- src/edu/jsu/mcis/cs310/tas_fa24/Punch.java
+++ src/edu/jsu/mcis/cs310/tas_fa24/Punch.java
@@ -1,5 +1,85 @@
+
 package edu.jsu.mcis.cs310.tas_fa24;
 
+import edu.jsu.mcis.cs310.tas_fa24.EventType;
+import java.time.LocalDateTime;
+import edu.jsu.mcis.cs310.tas_fa24.Badge;
+import edu.jsu.mcis.cs310.tas_fa24.PunchAdjustmentType;
+import java.sql.*;
+
+
 public class Punch {
+    private Integer id;  
+    private int terminalid;  
+    private Badge badge;  
+    private EventType punchType; 
+    private LocalDateTime originalTimestamp; 
+    private LocalDateTime adjustedTimestamp;  
+    private PunchAdjustmentType adjustmentType;
+
+    public void Punch(int terminalid, Badge badge, EventType punchType) {
+        this.terminalid = terminalid;
+        this.badge = badge;
+        // this.punchType = punchtype;
+        this.originalTimestamp = LocalDateTime.now();  
+        this.adjustedTimestamp = null;  
+        this.adjustmentType = null;  
+        
+    }
+    public void Punch(int id, int terminalid, Badge badge, LocalDateTime originaltimestamp, EventType punchType,LocalDateTime originalTimestamp, LocalDateTime adjustedTimestamp, 
+        PunchAdjustmentType adjustmentType) {
+        this.id = id;
+        this.terminalid = terminalid;
+        // this.punchType = punchtype;
+        this.originalTimestamp = originalTimestamp;
+        this.adjustedTimestamp = adjustedTimestamp;
+        this.adjustmentType = adjustmentType;
+    }
     
+    public Integer getId(){
+        return this.id;
+    }
+    public int getTerminalId(){
+        return this.terminalid;
+    }
+    public Badge getBadge(){
+        return this.badge;
+    }
+    public EventType getPunchType(){
+        return this.punchType;
+    }
+    public LocalDateTime getOriginalTimestamp(){
+        return this.originalTimestamp;
+    }
+    public LocalDateTime getAdjustedTimestamp(){
+        return this.adjustedTimestamp;
+    }
+    public PunchAdjustmentType getAdjustmentType(){
+        return this.adjustmentType;
+    }
+    public String printOriginal() {
+
+        StringBuilder s = new StringBuilder();
+        s.append("terminal id: ");
+        s.append('#').append(this.terminalid).append(' ');
+        
+        s.append("badge id: ");
+        s.append('#').append(this.badge).append(' ');
+        
+        s.append("timestamp: ");
+        //not sure if the conversion will work
+        s.append(this.originalTimestamp.toString()).append(' ');
+        
+        //s.append("event type: ");
+        //s.append(this.punchtype.toString()).append(' ');
+
+        return s.toString();
+
+    }
+    
+    @Override
+    public String toString(){
+       return printOriginal();
+       //to add here printAdjusted when it will be done
+    }
 }
--- src/edu/jsu/mcis/cs310/tas_fa24/dao/PunchDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa24/dao/PunchDAO.java
@@ -5,32 +5,79 @@ import edu.jsu.mcis.cs310.tas_fa24.EventType;
 import java.time.LocalDateTime;
 import edu.jsu.mcis.cs310.tas_fa24.Badge;
 import edu.jsu.mcis.cs310.tas_fa24.PunchAdjustmentType;
+import edu.jsu.mcis.cs310.tas_fa24.Punch;
+import java.sql.*;
+
 
 public class PunchDAO {
-    private Integer id;  
-    private int terminalid;  
-    private Badge badge;  
-    private EventType punchType; 
-    private LocalDateTime originalTimestamp; 
-    private LocalDateTime adjustedTimestamp;  
-    private PunchAdjustmentType adjustmentType;
+    private static final String QUERY_FIND = "SELECT * FROM event WHERE id = ?";
+    
+    private final DAOFactory daoFactory;
+    
+    PunchDAO(DAOFactory daoFactory) {
 
-    public void Punch(int terminalid, Badge badge, EventType punchType) {
-        this.terminalid = terminalid;
-        this.badge = badge;
-        // this.punchType = punchtype;
-        this.originalTimestamp = LocalDateTime.now();  
-        this.adjustedTimestamp = null;  
-        this.adjustmentType = null;  
-        
+        this.daoFactory = daoFactory;
     }
-    public void Punch(int id, int terminalid, Badge badge, LocalDateTime originaltimestamp, EventType punchType,LocalDateTime originalTimestamp, LocalDateTime adjustedTimestamp, 
-        PunchAdjustmentType adjustmentType) {
-        this.id = id;
-        this.terminalid = terminalid;
-        // this.punchType = punchtype;
-        this.originalTimestamp = originalTimestamp;
-        this.adjustedTimestamp = adjustedTimestamp;
-        this.adjustmentType = adjustmentType;
+    
+    public Punch find(String id) {
+
+        Punch punch = null;
+
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+
+            Connection conn = daoFactory.getConnection();
+
+            if (conn.isValid(0)) {
+
+                ps = conn.prepareStatement(QUERY_FIND);
+                ps.setString(1, id);
+
+                boolean hasresults = ps.execute();
+
+                if (hasresults) {
+
+                    rs = ps.getResultSet();
+
+                    while (rs.next()) {
+
+                        String terId = rs.getString("terminalid");
+                        //to complete here, ask about second Punch Constructor
+                        punch = new Punch(id, description);
+
+                    }
+
+                }
+
+            }
+
+        } catch (SQLException e) {
+
+            throw new DAOException(e.getMessage());
+
+        } finally {
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+
+        }
+
+        return punch;
+
     }
+
 }
