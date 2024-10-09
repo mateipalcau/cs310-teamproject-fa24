af854cadf4a5d4081dc747bfe268c33cc97502af
Matthew <matthewbright3000@gmail.com>
2024-10-09 11:58:14 -0500

    Shift Get And Set
    Added both a getter and setter function to the ShiftDAO.java class

--- src/edu/jsu/mcis/cs310/tas_fa24/dao/ShiftDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa24/dao/ShiftDAO.java
@@ -3,9 +3,27 @@ import java.time.*;
 import java.util.HashMap;
 
 public class ShiftDAO {
-    public LocalTime startShift, endShift, lunchDuration, shiftDuration;
+    private LocalTime startShift, endShift, lunchDuration, shiftDuration;
     
     public ShiftDAO(HashMap<String, LocalTime> data)
+    {
+        startShift = data.get("start");
+        endShift = data.get("end");
+        lunchDuration = data.get("lunch");
+        shiftDuration = data.get("shift");
+    }
+    
+    public HashMap getShift()
+    {   
+        HashMap<String, LocalTime> shiftData = new HashMap<>();
+        shiftData.put("start", startShift);
+        shiftData.put("end", endShift);
+        shiftData.put("lunch", lunchDuration);
+        shiftData.put("shift", shiftDuration);
+        return shiftData;
+    }
+    
+    public void setShift(HashMap<String, LocalTime> data)
     {
         startShift = data.get("start");
         endShift = data.get("end");
