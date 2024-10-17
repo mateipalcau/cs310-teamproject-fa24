b124391e32cb6286abc7a21d2b40857e637eda08
drummerjack03 <jcrawford8@stu.jsu.edu>
2024-10-17 09:22:54 -0500

    Update Shift.java

--- src/edu/jsu/mcis/cs310/tas_fa24/Shift.java
+++ src/edu/jsu/mcis/cs310/tas_fa24/Shift.java
@@ -1,5 +1,30 @@
 package edu.jsu.mcis.cs310.tas_fa24;
 
 public class Shift {
-    
+    private final String id, description;
+
+    public Shift(String id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder s = new StringBuilder();
+
+        s.append('#').append(id).append(' ');
+        s.append('(').append(description).append(')');
+
+        return s.toString();
+
+    }
 }
