a3cfeb9c25a05b45a902cdd679711b0a54d9a828
Garret <ghowell2@stu.jsu.edu>
2024-10-18 11:51:49 -0500

    removed auto javadoc

--- src/edu/jsu/mcis/cs310/tas_fa24/Department.java
+++ src/edu/jsu/mcis/cs310/tas_fa24/Department.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
 package edu.jsu.mcis.cs310.tas_fa24;
 
 /**
@@ -10,10 +6,4 @@ package edu.jsu.mcis.cs310.tas_fa24;
  */
 public class Department {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        // TODO code application logic here
-    }
 }
--- src/edu/jsu/mcis/cs310/tas_fa24/dao/DepartmentDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa24/dao/DepartmentDAO.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
 package edu.jsu.mcis.cs310.tas_fa24.dao;
 
 /**
@@ -9,11 +5,5 @@ package edu.jsu.mcis.cs310.tas_fa24.dao;
  * @author garre
  */
 public class DepartmentDAO {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        // TODO code application logic here
-    }
+    
 }
