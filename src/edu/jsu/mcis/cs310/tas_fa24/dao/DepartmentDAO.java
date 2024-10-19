9694ef3b7a25efc9c97cce62cc2a79eaa5367ab5
Garret <ghowell2@stu.jsu.edu>
2024-10-18 19:41:16 -0500

    Started work on DepartmentDAO.java and added getDepartmentDAO inside of DAOFactory

--- src/edu/jsu/mcis/cs310/tas_fa24/dao/DepartmentDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa24/dao/DepartmentDAO.java
@@ -1,9 +1,27 @@
 package edu.jsu.mcis.cs310.tas_fa24.dao;
-
-/**
- *
- * @author garre
- */
+import edu.jsu.mcis.cs310.tas_fa24.Department;
+import java.sql.*;
+import java.util.ArrayList;
 public class DepartmentDAO {
     
+    private final DAOFactory daoFactory;
+    
+    DepartmentDAO(DAOFactory daoFactory) {
+        this.daoFactory = daoFactory;
+    }
+    
+    public Department find(int id) {
+        Department depart = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Connection conn = null;
+        
+        try {
+
+            conn = daoFactory.getConnection();
+        } catch (Exception e) {
+        }
+        return depart;
+       
+    }
 }
