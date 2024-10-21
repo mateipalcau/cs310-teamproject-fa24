1bf541554e370011fa2c5110b97642fa862c7090
Matthew <matthewbright3000@gmail.com>
2024-10-21 12:12:08 -0500

    ShiftDAO ready for query
    ShiftDAO class is ready for the query to be written

--- src/edu/jsu/mcis/cs310/tas_fa24/dao/ShiftDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa24/dao/ShiftDAO.java
@@ -9,10 +9,9 @@ public class ShiftDAO {
     private static final String QUERY = "";//find shift sql query here
     private final DAOFactory daoFactory;
     
-    public Shift find(HashMap<String, LocalTime> data)//set parameter to defining data field
+    public Shift find(String id)//set parameter to defining data field
     {
         Shift shift = null;
-        String startShift = data.get("start").toString();
         PreparedStatement ps = null;
         ResultSet rs = null;
         
@@ -24,7 +23,7 @@ public class ShiftDAO {
             if (conn.isValid(0)) {
 
                 ps = conn.prepareStatement(QUERY);
-                ps.setString(1, startShift);//key
+                ps.setString(1, id);//key
 
                 boolean hasresults = ps.execute();
 
@@ -35,7 +34,8 @@ public class ShiftDAO {
                     while (rs.next()) {
 
                         String description = rs.getString("description");
-                        shift = new Shift(startShift, description);//key
+                        //idk man
+                        shift = new Shift();
 
                     }
 
