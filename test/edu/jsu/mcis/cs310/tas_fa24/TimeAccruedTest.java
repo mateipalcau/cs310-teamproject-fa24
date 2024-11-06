47a980fff44a90085514cfadccbe22b802060385
Garret <ghowell2@stu.jsu.edu>
2024-11-06 12:00:53 -0600

    Finished time accrued tests 5/5 passing

--- test/edu/jsu/mcis/cs310/tas_fa24/TimeAccruedTest.java
+++ test/edu/jsu/mcis/cs310/tas_fa24/TimeAccruedTest.java
@@ -136,5 +136,32 @@ public class TimeAccruedTest {
         assertEquals(540, m);
 
     }
+    @Test
+    public void testMinutesAccrued() throws SQLException {
+        PunchDAO punchDAO = daoFactory.getPunchDAO();
+        ShiftDAO shiftDAO = daoFactory.getShiftDAO();
 
+        /* Get Punch/Badge/Shift Objects */
+
+        Punch p = punchDAO.find(4944);
+        Badge b = p.getBadge();
+        Shift s = shiftDAO.find(b);
+        
+        /* Get/Adjust Punch List */
+
+        ArrayList<Punch> dailypunchlist = punchDAO.list(b, p.getOriginalTimestamp().toLocalDate());
+
+        for (Punch punch : dailypunchlist) {
+            punch.adjust(s);
+        }
+
+        /* Compute Pay Period Total */
+        
+        int m = DAOUtility.calculateTotalMinutes(dailypunchlist, s);
+
+        /* Compare to Expected Value */
+        
+        assertEquals(600, m);
+
+    }
 }
