6fbe39c87a6d05773496ef2621a96e5dbdb502d7
Matei Palcau <mpalcau@stu.jsu.edu>
2024-11-11 12:06:28 -0600

    I fixed the error for absenteeism. Now, all the tests pass

--- src/edu/jsu/mcis/cs310/tas_fa24/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/tas_fa24/dao/DAOUtility.java
@@ -40,7 +40,8 @@ public final class DAOUtility {
         return Jsoner.serialize(jsonData);
     }
     public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, Shift shift) {
-        ArrayList<Integer> totalMinutes = new ArrayList<Integer>();
+        int totalMinutes=0;
+        ArrayList<Punch> daily = new ArrayList<Punch>();
         int sum=0;
         
         boolean clockInFound = false;
@@ -50,25 +51,36 @@ public final class DAOUtility {
             if (punch.getPunchType() == EventType.CLOCK_IN) {
                 clockInPunch = punch;
                 clockInFound = true;
+                daily.add(punch);
             } 
             else if (punch.getPunchType() == EventType.CLOCK_OUT && clockInFound) {
                 int minutesWorked = (int) ChronoUnit.MINUTES.between(clockInPunch.getAdjustedTimestamp(), punch.getAdjustedTimestamp());
-                totalMinutes.add(minutesWorked);
+                if(clockInPunch.getAdjustedTimestamp().toLocalTime().compareTo(shift.getLunchStart())==0 && clockInPunch.getAdjustedTimestamp().toLocalDate().getDayOfWeek().compareTo(DayOfWeek.SATURDAY)!=0 &&
+                clockInPunch.getAdjustedTimestamp().toLocalDate().getDayOfWeek().compareTo(DayOfWeek.SUNDAY)!=0){
+                    daily.add(punch);
+                }else{
+                    daily.add(punch);
+                    
+                    if(minutesWorked >= shift.getLunchThreshold() && hasClockOutDuringLunch(daily,shift)==true){
+                        minutesWorked=minutesWorked-shift.getLunchDuration();
+                        totalMinutes=totalMinutes+minutesWorked;
+                    }else{
+                        totalMinutes=totalMinutes+minutesWorked;
+                    }
+                   
+                    daily.clear();
+                }
+                
                 clockInFound = false;
             } 
             else if (punch.getPunchType() == EventType.TIME_OUT) {
                 clockInFound = false;
+                daily.add(punch);
             }
         }
-        for(int i: totalMinutes){
-           if (i > shift.getLunchThreshold() && !hasClockOutDuringLunch(dailypunchlist, shift)) {
-               i -= shift.getLunchDuration();
-               sum=sum+i;
-           }else{
-               sum=sum+i;
-           }
-        }
-        return sum;
+        
+        
+        return totalMinutes;
     }
 
     //method that checks if a lunch break was actually taken
@@ -77,9 +89,9 @@ public final class DAOUtility {
             Punch current = dailypunchlist.get(i);
             Punch next = dailypunchlist.get(i + 1);
             //this checks if a clock out is followed by a clock in during lunch
-            if (current.getPunchType() == EventType.CLOCK_OUT &&
+            if (current.getPunchType() == EventType.CLOCK_IN &&
                 current.getAdjustedTimestamp().toLocalTime().isBefore(shift.getLunchStop()) &&
-                next.getPunchType() == EventType.CLOCK_IN &&
+                next.getPunchType() == EventType.CLOCK_OUT &&
                 next.getAdjustedTimestamp().toLocalTime().isAfter(shift.getLunchStart()) && 
                 next.getAdjustedTimestamp().toLocalDate().getDayOfWeek().compareTo(DayOfWeek.SATURDAY)!=0 &&
                 current.getAdjustedTimestamp().toLocalDate().getDayOfWeek().compareTo(DayOfWeek.SATURDAY)!=0) {
